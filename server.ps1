10d0f6ad77a0aac60eff24bec64960f222b59e02
Jonathan <mrjonathanbland@gmail.com>
2020-07-20 10:24:51 -0500

    Update server.ps1
    added put listener to stop server.

--- server.ps1
+++ server.ps1
@@ -1,23 +1,35 @@
 $http = [System.Net.HttpListener]::new()
-$http.Prefixes.Add("http://localhost:8080/")
+$http.Prefixes.Add("http://*:80/")
 $http.Start()
 
 if ($http.IsListening) {
-   write-host " HTTP Server Ready!  " -f 'black' -b 'gre'
-   write-host "now try going to $($http.Prefixes)" -f 'y'
-   write-host "then try going to $($http.Prefixes)other/path" -f 'y'
-}
 
-while ($http.IsListening) {
-   $context = $http.GetContext()
-   if ($context.Request.HttpMethod -eq 'GET') {
-      write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
-      [string]$html = "<h1>Hello World</h1>"
-      $buffer = [System.Text.Encoding]::UTF8.GetBytes($html) 
-      $context.Response.ContentLength64 = $buffer.Length
-      $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) 
-      $context.Response.OutputStream.Close() 
+   write-host "Ready Go!"
+
+   while ($http.IsListening) {
+      $context = $http.GetContext()
+      if ($context.Request.HttpMethod -eq 'GET') {
+         $timeStamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
+         write-host "$($timeStamp)`t$($context.Request.UserHostAddress)`t$($context.Request.Url)" -f 'green'
+         $timeStamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
+         [string]$html = "<h1>Hello World</h1><p>$($timeStamp)</>"
+         $buffer = [System.Text.Encoding]::UTF8.GetBytes($html) 
+         $context.Response.ContentLength64 = $buffer.Length
+         $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) 
+         $context.Response.OutputStream.Close() 
+      }
+      elseif ($context.Request.HttpMethod -eq 'PUT') {
+         $message = "$($timeStamp)`t$($context.Request.UserHostAddress)`t Stop Request"
+         write-host $message -f 'red';
+         $buffer = [System.Text.Encoding]::UTF8.GetBytes($message) 
+         $context.Response.ContentLength64 = $buffer.Length
+         $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) 
+         $context.Response.OutputStream.Close()
+         $http.Stop()
+      }
 
    }
 
 }
+
+$http.Dispose()
